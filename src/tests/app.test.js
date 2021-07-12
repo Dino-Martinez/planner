747192bc942b422f79167583c9ebf666e25be800
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-07-11 21:13:20 -0700

    [update] index.js app.test.js
    Added simple testing and routes for viewing or adding TODOs

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -2,8 +2,18 @@ const request = require('supertest')
 const app = require('../app')
 
 describe('Test app.js', () => {
-  it('Should respond to the GET method', async () => {
+  it('Should return array of todos', async () => {
     const response = await request(app).get('/')
     expect(response.statusCode).toBe(200)
+    expect(response.body).toBeDefined()
+    expect(response.body).toBeInstanceOf(Array)
+  })
+
+  it('Should add a todo', async () => {
+    const success = await request(app).post('/').send({ _id: 0x000, title: 'This is a new todo', body: 'This todo has a body' })
+    const response = await request(app).get('/')
+    expect(response.statusCode).toBe(200)
+    expect(success.body).toEqual(true)
+    expect(response.body).toHaveLength(1)
   })
 })
