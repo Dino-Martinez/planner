bea4c186c7108b54773d82a24ca6ab66bdf28635
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-07-12 21:26:48 -0700

    [update] app.test.js index.js
    Added tests and routes for CRUD on a TODO

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,48 @@
-// Mongood models
+// Mongoose models
 const Todo = require('../models/todos')
 
 module.exports = app => {
-  app.get('/', (req, res) => {
-    Todo.find().lean().then(todos => {
-      res.json(todos)
-    })
-  })
-
+  // Create a new todo
   app.post('/', (req, res) => {
     const todo = new Todo(req.body)
     todo.save().then(doc => {
       res.send({ todo: doc })
     }).catch(err => {
       res.status(400).send(err.errors)
+    })
+  })
+
+  // Read all todos
+  app.get('/', (req, res) => {
+    Todo.find().lean().then(todos => {
+      res.json(todos)
+    }).catch(err => {
+      res.status(400).send(err.errors)
+    })
+  })
+
+  // Read one todo
+  app.get('/:id', (req, res) => {
+    Todo.findById(req.params.id).lean().then(todo => {
+      res.json(todo)
+    }).catch(err => {
+      res.status(400).send(err.errors)
+    })
+  })
+
+  // Update one todo
+  app.put('/:id', (req, res) => {
+    Todo.findByIdAndUpdate(req.params.id, req.body).then(doc => {
+      res.send(doc)
+    }).catch(err => {
+      res.status(400).send(err.errors)
+    })
+  })
+
+  // Delete one todo
+  app.delete('/:id', (req, res) => {
+    Todo.findByIdAndDelete(req.params.id).then(() => {
+      res.json({ success: true })
     })
   })
 }
--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -3,7 +3,7 @@ const assert = chai.assert
 const chaiHttp = require('chai-http')
 const app = require('../app')
 const Todo = require('../models/todos')
-
+const SAMPLE_OBJECTID = '60ed0eaf1df2ba3be8646211'
 // Configure chai
 chai.use(chaiHttp)
 chai.should()
@@ -11,13 +11,22 @@ chai.should()
 describe('Test app.js', () => {
   // Before and after are only defined within the context of the Mocha runtime environment
   // eslint-disable-next-line no-undef
-  before(function () {
-    console.log('Running tests...')
+  before(async function () {
+    const todo = new Todo({ _id: SAMPLE_OBJECTID, title: 'This is the first todo', body: 'this body is meaningless' })
+    await todo.save()
   })
 
   // eslint-disable-next-line no-undef
   after(async function () {
     await Todo.deleteMany({})
+  })
+
+  it('Should add a todo', async function () {
+    const success = await chai.request(app).post('/').send({ title: 'This is a new todo', body: 'This todo has a body' })
+    const response = await chai.request(app).get('/')
+    assert.strictEqual(response.statusCode, 200, 'Reponse code success')
+    assert.isObject(success.body, 'Success response received')
+    assert.lengthOf(response.body, 2, 'Array has two todos')
   })
 
   it('Should return array of todos', async function () {
@@ -27,11 +36,28 @@ describe('Test app.js', () => {
     assert.isArray(response.body)
   })
 
-  it('Should add a todo', async function () {
-    const success = await chai.request(app).post('/').send({ title: 'This is a new todo', body: 'This todo has a body' })
-    const response = await chai.request(app).get('/')
-    assert.strictEqual(response.statusCode, 200, 'Reponse code success')
+  it('Should return a single todo', async function () {
+    const response = await chai.request(app).get(`/${SAMPLE_OBJECTID}`)
+    assert.strictEqual(response.statusCode, 200, 'Response code success')
+    assert.isDefined(response.body, 'Response body is defined')
+    assert.isObject(response.body)
+    assert.strictEqual(response.body.title, 'This is the first todo', 'The correct todo was returned')
+  })
+
+  it('Should update a single todo', async function () {
+    const success = await chai.request(app).put(`/${SAMPLE_OBJECTID}`).send({ title: 'This is an updated todo' })
+    const response = await chai.request(app).get(`/${SAMPLE_OBJECTID}`)
     assert.isObject(success.body, 'Success response received')
-    assert.isNotEmpty(response.body, 'Array has at least one todo')
+    assert.strictEqual(response.statusCode, 200, 'Response code success')
+    assert.isDefined(response.body, 'Response body is defined')
+    assert.isObject(response.body)
+    assert.strictEqual(response.body.title, 'This is an updated todo', 'The correct todo was returned')
+  })
+
+  it('Should delete a single todo', async function () {
+    const success = await chai.request(app).delete(`/${SAMPLE_OBJECTID}`)
+    const response = await chai.request(app).get(`/${SAMPLE_OBJECTID}`)
+    assert.isObject(success.body, 'Success response received')
+    assert.strictEqual(response.statusCode, 200, 'Response code success')
   })
 })
